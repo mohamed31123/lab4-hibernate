0009f013dc47207efcc68c71c5894bcd4ebe6480
mohamed <m.eddinari5197@uca.ac.ma>
2026-03-09 22:43:00 +0100

    the 1st commit

---
+++ src/main/java/entities/Machine.java
@@ -0,0 +1,75 @@
+package entities;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Entity
+@NamedNativeQuery(name = "findBetweenDateNative", query = "select * from machine where dateAchat between :d1 and :d2", resultClass = Machine.class)
+@NamedQuery(name = "findBetweenDate", query = "from Machine where dateAchat between :d1 and :d2")
+public class Machine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String ref;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateAchat;
+
+    @ManyToOne
+    private Salle salle;
+
+    // Constructeurs, getters et setters
+
+
+    public Machine() {
+    }
+
+    public Machine( String ref, Date dateAchat,Salle salle ) {
+        this.dateAchat = dateAchat;
+        this.salle = salle;
+        this.ref = ref;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public Date getDateAchat() {
+        return dateAchat;
+    }
+
+    public void setDateAchat(Date dateAchat) {
+        this.dateAchat = dateAchat;
+    }
+
+    public Salle getSalle() {
+        return salle;
+    }
+
+    public void setSalle(Salle salle) {
+        this.salle = salle;
+    }
+
+    @Override
+    public String toString() {
+        return "Machine{" +
+                "id=" + id +
+                ", ref='" + ref + '\'' +
+                ", dateAchat=" + dateAchat +
+                ", salle=" + salle +
+                '}';
+    }
+}
